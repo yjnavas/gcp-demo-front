97d5ceb04b1a282bf18415a3a4335aaaab288ef7
Yovani Navas <yovaninavas10@gmail.com>
2026-01-28 15:48:20 -0400

    excluir pruebas e2e en vitest

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,4 +1,4 @@
-import { defineConfig } from 'vitest/config';
+import { defineConfig, configDefaults } from 'vitest/config';
 import react from '@vitejs/plugin-react';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
@@ -8,5 +8,8 @@ export default defineConfig({
     environment: 'jsdom',
     globals: true,
     setupFiles: './vitest.setup.ts', // Opcional: para configuraciones globales
+    // Mantenemos las exclusiones por defecto (node_modules, dist, etc.)
+    // y agregamos tu carpeta de pruebas E2E.
+    exclude: [...configDefaults.exclude, 'tests-e2e/**'],
   },
 });
